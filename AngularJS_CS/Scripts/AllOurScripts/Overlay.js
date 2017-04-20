5eeb02f7d7b17796888d3992f286053f8641f951
kabolas <suri.mur.bry@gmail.com>
2017-04-20 13:39:57 +0200

    Ajout du Sidenav Overlay

---
+++ AngularJS_CS/Scripts/AllOurScripts/Overlay.js
@@ -0,0 +1,11 @@
+﻿/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("Sidenav").style.width = "25%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+ 
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("Sidenav").style.width = "0";
+    document.body.style.backgroundColor = "white";
+}
